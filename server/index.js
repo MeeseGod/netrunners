77ebb868e987679616599588892fd98d72db837e
JosephR <joseph_rundell@outlook.com>
2022-11-19 21:57:59 -0500

    Cleaned up React files and changed around index.js for the server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const port = process.env.PORT || 1234;
 
 app.get("/", (req, res) => {
     res.send("testing")
 })
 
-app.listen(1234);
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+app.listen(port, ()=>{console.log(`Listening on port: ${port}`)});
