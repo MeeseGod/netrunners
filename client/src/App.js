47e429592e37bff1d17b4051576aa8ef01085428
JosephR <joseph_rundell@outlook.com>
2022-11-21 02:22:32 -0500

    Implemented routing, created pages folder and homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home";
+
 export default function App() {
   return (
-    <div className="App">
-      <h1>Netrunners</h1>
-    </div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Routes>
+      <Route path="/" exact element={<Home/>} />
+    </Routes>
+  </BrowserRouter>
   );
 }
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-const {
-    getUsers,
-} = require("../controllers/userController");
+const userController = require("../controllers/userController");
 
-router.get("/", getUsers);
+router.get("/", userController.getUsers);
 
 
 module.exports = router;
