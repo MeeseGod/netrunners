ebcfc7a302e0b90252bac49163321b54f8bf6b51
JosephR <joseph_rundell@outlook.com>
2022-12-05 22:31:48 -0500

    Start of implementation of conditional rendering for mission components

--- client/src/components/Mission.js
+++ client/src/components/Mission.js
@@ -2,10 +2,29 @@ import Timer from "../components/Timer";
 
 export default function Mission(props){
     const mission = props.mission;
-    console.log(props.mission);
+    // console.log(props.mission);
+
+    function calculateTime(){
+        const currentTime = Date.now() / 1000;
+        const endTime = (Date.now() + 60000) / 1000;
+        // Will be used when endTimes are properly implemented
+        // const endTime = mission.endTime / 1000;
+        return endTime - currentTime;
+    };
+
+    function startMission(){
+        console.log("Start Mission");
+    };
+
 
     return <div className="missionContainer">
         <h2>{mission.title}</h2>
-        <Timer time={30}/>
+        <div>
+            {mission.isStarted
+                ? <Timer time={calculateTime()}/>
+                : <button onClick={() => {startMission()}}>Start Mission</button>
+            }
+        </div>
+
     </div>
 };
